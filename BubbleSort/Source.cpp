02b8ed6419678bfd5bcfc1a92b78b299986484d7
Cozinia <hdwallpaper04@gmail.com>
2023-03-07 17:39:14 +0200

    Am adaugat BubbleSort

---
+++ BubbleSort/Source.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int swap(int& a, int& b) {
+	int aux = a;
+	a = b;
+	b = aux;
+
+	return a, b;
+}
+
+void bubleSort(int arr[], int size) {
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
+		{
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+
+		}
+	}
+	return;
+}
+
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+
+
+int main() {
+	const int size = 7;
+	int arr[size] = { 2, 8, 5, 3, 9, 4, 1 };
+	bubleSort(arr, size);
+	printArray(arr, size);
+	return 0;
+}
